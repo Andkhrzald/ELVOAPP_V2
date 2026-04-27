058fd5e4edf62e11f1e6bfafebf7fd161f07e468
Rehanfzn <rehanfaezan30@gmail.com>
2026-04-28 03:00:44 +0700

    new file 2

---
+++ resources/views/customer/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.customer') {{-- Pake layout khusus customer --}}
+
+@section('content')
+<section class="hero">
+    <h1>Selamat Datang di ELVOAPP</h1>
+    <p>Temukan produk terbaik kami di sini!</p>
+</section>
+
+<div class="products">
+    {{-- Loop produk di sini --}}
+</div>
+@endsection
