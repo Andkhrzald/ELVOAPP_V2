d22fe1298894da48c7e742b8433e8135bc304c4e
Rehanfzn <rehanfaezan30@gmail.com>
2026-04-28 03:25:03 +0700

    new folder

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\AdminController;
 
 // ROUTE UNTUK CUSTOMER (Halaman Depan)
-Route::get('/', [CustomerController::class, 'index'])->name('home');
-Route::get('/product/{id}', [CustomerController::class, 'show'])->name('product.detail');
+Route::get('/', function () {
+    return view('customer.home'); // Pastikan file home.blade.php sudah ada di folder views/customer
+})->name('home');
 
-// ROUTE UNTUK ADMIN (Diberi prefix 'admin' agar URL-nya jadi /admin/dashboard)
+// ROUTE UNTUK ADMIN
 Route::prefix('admin')->group(function () {
-Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard'); // Pastikan file dashboard.blade.php sudah ada di folder views/admin
+    })->name('admin.dashboard');
 });
